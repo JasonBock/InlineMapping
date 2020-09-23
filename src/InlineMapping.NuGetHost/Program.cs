3766eb85f3b4c86381708b238d21adcd59ac8ac7
Jason Bock <jason@jasonbock.net>
2020-09-23 14:53:37 -0500

    Finally got the packaging to work....I think

---
+++ src/InlineMapping.NuGetHost/Program.cs
@@ -0,0 +1,24 @@
+﻿using InlineMapping;
+using System;
+
+var source = new Source { Id = Guid.NewGuid(), Name = "Joe", When = DateTime.Now };
+var destination = source.MapToDestination();
+
+Console.Out.WriteLine(destination.Id);
+Console.Out.WriteLine(destination.Name);
+Console.Out.WriteLine(destination.When);
+
+[MapTo(typeof(Destination))]
+public class Source
+{
+	public Guid Id { get; set; }
+	public string? Name { get; set; }
+	public DateTime When { get; set; }
+}
+
+public class Destination
+{
+	public Guid Id { get; set; }
+	public string? Name { get; set; }
+	public DateTime When { get; set; }
+}
